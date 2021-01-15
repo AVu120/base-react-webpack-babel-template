ecce76a401fbc74250f3292d715f26a8ec48c209
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-01-16 10:14:28 +1100

    Enable rendering of react through babel-react preset.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Hello Webpack Project.");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "React with Webpack and Babel";
+
+ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
