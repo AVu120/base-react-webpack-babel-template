e265221a3f4c707afc5de1cb5826dc222e52055e
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-01-16 12:53:12 +1100

    Create environment variables via webpack and only enable HMR in development mode.

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,9 +1,17 @@
 const webpack = require("webpack");
 const path = require("path");
+const { DefinePlugin } = require("webpack");
 
 module.exports = {
   mode: "development",
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development"),
+      },
+    }),
+  ],
   devServer: {
     contentBase: path.resolve(__dirname, "./dist"),
     hot: true,
--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ const title = "React with Webpack and Babel";
 ReactDOM.render(<App title={title} />, document.getElementById("app"));
 
 // Comment below before building for production.
-// module.hot.accept();
+process.env.NODE_ENV === "development" && module.hot.accept();
