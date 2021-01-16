9b5c896e4ab377740b2cd4ffd612208cbce7c27a
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-01-16 11:53:23 +1100

    Enable automatic regeneration of dist/index.html on 'npm run build'.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require("webpack");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "./src/index.js"),
@@ -19,7 +20,13 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js",
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Base React Webpack Babel Template",
+      template: path.resolve(__dirname, "./src/index.html"),
+    }),
+  ],
   devServer: {
     contentBase: path.resolve(__dirname, "./dist"),
     hot: true,
